62d026c8ff9d59a63b836ddfec20e0e11da21833
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2020-02-19 20:57:22 +0530

    The app.js is updated

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-let re;
-re = /swapnil/i;
-str = "hello swapnil";
+const weatherLocation = storage.getLocationData();
 
-const result = str.search("swapnil");
-console.log(result);
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+const ui = new UI();
+
+document.addEventListener("DOMContentLoaded", getWeather);
+
+document.getElementById("w-change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+
+  weather.changeLocation(city, state);
+
+  storage.setLocationData(city, state);
+
+  getWeather();
+
+  $("#locModal").modal("hide");
+});
+
+function getWeather() {
+  weather
+    .getWeather()
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
